083e7fa831c26ad2a9e83a2de99bc1b9defa8580
Kavya Singampalli <kavyasingampalli@Kavyas-MacBook-Air.local>
2025-09-01 20:06:31 +0530

    success

--- src/screens/Success.tsx
+++ src/screens/Success.tsx
@@ -16,8 +16,8 @@ export const Success = () => {
                 <View style={{ marginTop: rsHeight(95) - insets.top }}>
                     <Text style={styles.title}>You're<Text style={styles.secondryColor}>All Set!</Text></Text>
                     <Text style={styles.subTitle}>We'll send you OTP for Email verification</Text>
-                    <View style={{alignItems:"center",marginTop:rsHeight(180)}}>
-                        <Image style={{width:rsWidth(117.79),height:rsHeight(121.43)}} source={require('../../assets/success-icon.png')}></Image>
+                    <View style={{ alignItems: "center", marginTop: rsHeight(180) }}>
+                        <Image style={{ width: rsWidth(117.79), height: rsHeight(121.43) }} source={require('../../assets/success-icon.png')}></Image>
                     </View>
                 </View>
             </View>
@@ -28,25 +28,25 @@ export const Success = () => {
 
 const styles = StyleSheet.create({
     container: {
-            flex: 1,
-            padding: Spacing.lg,
-            justifyContent: "space-between",
-            paddingBottom: 0
-        },
-        title: {
-            fontFamily: typography.heading2.fontFamily,
-            color: theme.colors.primary,
-            fontSize: typography.heading2.fontSize,
-            fontWeight: typography.heading2.fontWeight
-        },
-        secondryColor: {
-            color: theme.colors.secondary
-        },
-        subTitle: {
-            color: theme.colors.input,
-            fontSize: typography.subTitle.fontSize,
-            fontFamily: typography.subTitle.fontFamily,
-            fontWeight: typography.subTitle.fontWeight,
-            marginTop: Spacing.xs
-        },
+        flex: 1,
+        padding: Spacing.lg,
+        justifyContent: "space-between",
+        paddingBottom: 0
+    },
+    title: {
+        fontFamily: typography.heading2.fontFamily,
+        color: theme.colors.primary,
+        fontSize: typography.heading2.fontSize,
+        fontWeight: typography.heading2.fontWeight
+    },
+    secondryColor: {
+        color: theme.colors.secondary
+    },
+    subTitle: {
+        color: theme.colors.input,
+        fontSize: typography.subTitle.fontSize,
+        fontFamily: typography.subTitle.fontFamily,
+        fontWeight: typography.subTitle.fontWeight,
+        marginTop: Spacing.xs
+    },
 })
